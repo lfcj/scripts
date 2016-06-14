a1c0ba21fcdcdfc2f997d39bfc267f448eeec233
lfcj <fernanda16400@gmail.com>
2016-06-14 19:26:54 +0200

    add correction boxes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var colors = ["#E1E1FF", "#DBEBFF", "#ECFAFF", "#D6F8DE", "#FFECFF"];
+var colors = ["#E1E1FF", "#FFECFF","#DBEBFF", "#ECFAFF", "#D6F8DE"];
 var amount_colors = colors.length;
 
 function show_correction(){
@@ -6,30 +6,25 @@ function show_correction(){
 }
 
 function isHighlighted(sentence){
-	var isHighlighted = true;
-	var i = 0;
-	while (isHighlighted && i<amount_colors) {
-		isHighlighted = isHighlighted && sentence.style.backgroundColor == colors[i];
-		i++;
-	}
+	var notSet = sentence.style.backgroundColor === "";
+	var isTransparent = sentence.style.backgroundColor === "transparent"; 
+	var isHighlighted = !isTransparent && !notSet;
 	return isHighlighted;
 }
-function highlight(){
+function showOrHide(){
 	var correction_sentences = document.querySelectorAll(".correction");
-	var correction_boxes = document.querySelectorAll(".correction_box");
-	
-
-
+	var correction_boxes = document.querySelectorAll(".correction-box");
 	// make sure there are as many highlighted sentences as corrections.
-	if (correction_sentences.length == correction_boxes.length){
+	if (correction_sentences.length === correction_boxes.length){
 		for (var i = 0;i<correction_sentences.length;i++){
 		if (isHighlighted(correction_sentences[i])){
 			correction_sentences[i].style.backgroundColor = "transparent";
-			correction_boxes[i].style.display = "none";
+			correction_boxes[i].style.visibility = "hidden";
 		} else {
 			correction_sentences[i].style.backgroundColor = colors[i%amount_colors];
-			correction_boxes[i].style.display = "block";
-			correction_sentences.style.borderColor = colors[i%amount_colors];
+			correction_boxes[i].style.visibility = "visible";
+			correction_boxes[i].style.borderColor = colors[i%amount_colors];
+			correction_boxes[i].style.backgroundColor = colors[i%amount_colors];
 		}
 	}	
 	}
