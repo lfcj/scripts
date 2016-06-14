2db7f0353054e721554ac5d1b69abc2671dce677
lfcj <fernanda16400@gmail.com>
2016-06-14 18:27:26 +0200

    highlight in different colors

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ function highlight(){
 	console.log(Object.keys(correction_elements[0]));
 	var i = 0;
 	for (i;i<correction_elements.length;i++){
-		correction_elements[i].style.backgroundColor=="transparent"?"green":"transparent";
+		if (correction_elements[i].style.backgroundColor=="green"){
+			correction_elements[i].style.backgroundColor = "transparent";
+		} else {
+			correction_elements[i].style.backgroundColor = "green";
+		}
 	}
-	console.log(Object.keys(correction_elements[0].style));
-	console.log("hi");
 }
